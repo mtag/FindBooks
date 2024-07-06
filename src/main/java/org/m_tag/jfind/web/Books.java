d8de3e5b3af09eee1e83c162fd7e3f7ad4562020
Masanao TAGUCHI <mtag@jp.ibm.com>
2024-07-06 22:08:20 +0900

    move init config from Books to Application and format codes

--- src/main/java/org/m_tag/jfind/web/Books.java
+++ src/main/java/org/m_tag/jfind/web/Books.java
@@ -1,77 +1,73 @@
-/*******************************************************************************
- * (c) Copyright IBM Corporation 2017.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/
 package org.m_tag.jfind.web;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
-
-import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
-
 import org.m_tag.jfind.books.Book;
 import org.m_tag.jfind.books.Config;
 import org.m_tag.jfind.books.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//  curl http://192.168.200.190:9080/FindBooks/books --get --data-urlencode author=
+/**
+ * Resource of api To find books.
+ * <p/>
+ * 
+ * <pre>
+ * curl http://192.168.200.190:9080/FindBooks/books --get --data-urlencode author=${AUTHOR}
+ * </pre>
+ */
 @Path("/books")
 public class Books {
+  /**
+   * logger.
+   */
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-	
-	@Context
-	private ServletContext context;
+  /**
+   * books finder.
+   */
+  private final Config finders;
 
-	private Config config;
+  /**
+   * constructor.
+   *
+   * @param app application class.
+   */
+  public Books(final FindApplication app) {
+    super();
+    this.finders = app.getFinders();
+  }
 
-	@GET
-	@Produces("application/json")
-	public Response get(@QueryParam("author") final String author, @QueryParam("title") final String title,
-			@QueryParam("keyword") final String keyword) throws IOException, ClassNotFoundException, SQLException {
+  /**
+   * find books with finders.
+   *
+   * @param author author of book(set null if not specified).
+   * @param title title of book(set null if not specified).
+   * @param keyword keyword(any of fields) of book(set null if not specified).
+   * @return list of books(returns empty if found nothing).
+   * @throws IOException error in finding books
+   * @throws SQLException error in JDBC query.
+   */
+  @GET
+  @Produces("application/json")
+  public Response get(@QueryParam("author") final String author,
+      @QueryParam("title") final String title, @QueryParam("keyword") final String keyword)
+      throws IOException, SQLException {
 
-		this.config = getConfig();
-
-		final Query query = new Query();
-		query.setAuthor(author);
-		query.setTitle(title);
-		query.setKeyword(keyword);
-		query.setExists(false);
-		logger.info("finding: {}", query);
-		List<Book> books = config.find(query).toList();
-		logger.info("found: size:{}", books.size());
-		return Response.status(Response.Status.OK).entity(books).build();
-	}
-
-	private Config getConfig() throws IOException {
-		if (config == null) {
-			String file = System.getenv().get(Config.JFINDBOOKS_JSON);
-			if (file == null || file.length() == 0) {
-				file = context.getRealPath("/WEB-INF/jFindBooks.json");
-			}
-			logger.info("loaing config : {}", file);
-			config = Config.getConfig(file);
-			logger.info("loaded config : {} : {}", file, config);
-		}
-		return config;
-	}
+    final Query query = new Query();
+    query.setAuthor(author);
+    query.setTitle(title);
+    query.setKeyword(keyword);
+    query.setExists(false);
+    logger.info("finding: {}", query);
+    List<Book> books = finders.find(query).toList();
+    logger.info("found: size:{}", books.size());
+    return Response.status(Response.Status.OK).entity(books).build();
+  }
 }
--- src/main/java/org/m_tag/jfind/web/FindApplication.java
+++ src/main/java/org/m_tag/jfind/web/FindApplication.java
@@ -1,16 +1,113 @@
 package org.m_tag.jfind.web;
 
+import java.io.IOException;
 import java.util.HashSet;
+import java.util.MissingResourceException;
 import java.util.Set;
-
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import org.m_tag.jfind.books.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Application class for books.
+ * <p/>
+ * JFINDBOOKS_JSON must be declared.
+ * <dl>
+ * <dt>environment variable</dt>
+ * <dd>set JFINDBOOKS_JSON for path of config json. This is first choice for the value.</dd>
+ * <dt>JNDI</dt>
+ * <dd>set JNDI description on your server.xml.
+ * 
+ * <pre>
+ *    <server>
+ *     <jndiEntry
+ *        jndiName="FindBooks/JsonPath"
+ *        value='"C:\\Users\\mtag\\eclipse-workspace\\FindBooks\\jFindBooks.json"' />
+ *    </server>
+ * </pre>
+ * 
+ * </dd>
+ * </dl>
+ */
 @ApplicationPath("find")
 public class FindApplication extends Application {
-	  @Override
-	  public Set<Class<?>> getClasses() {
-	    Set<Class<?>> classes = new HashSet<>();
-	    classes.add( org.m_tag.jfind.web.Books.class);
-	    return classes;
-	  }
+  /**
+   * logger.
+   */
+  private static final Logger logger = LoggerFactory.getLogger(FindApplication.class);
+
+  /**
+   * books finder.
+   */
+  private Config finders;
+
+  /**
+   * constructor.
+   *
+   * @throws IOException Error in reading configuration json.
+   */
+  public FindApplication() throws IOException {
+    super();
+    String file = getConfigFileName();
+    if (file == null) {
+      throw new MissingResourceException("Cannot find JFindBooks.json",
+          FindApplication.class.toString(), "JFINDBOOKS_JSON");
+    }
+    this.finders = loadConfig(file);
+  }
+
+  @Override
+  public Set<Object> getSingletons() {
+    Set<Object> singletons = new HashSet<>();
+    singletons.add(new Books(this));
+    return singletons;
+  }
+
+  /**
+   * get books finders.
+   *
+   * @return finders
+   */
+  Config getFinders() {
+    return finders;
+  }
+
+  /**
+   * get config json file name.
+   *
+   * @return file name of json config, or null if not defined.
+   */
+  private static String getConfigFileName() {
+    String file = System.getenv().get(Config.JFINDBOOKS_JSON);
+    if (file == null || file.length() == 0) {
+      try {
+        final InitialContext ic = new InitialContext();
+        final Object value = ic.lookup("FindBooks/JsonPath");
+        if (value instanceof String str) {
+          file = str;
+        }
+      } catch (NamingException ex) {
+        ex.printStackTrace();
+      }
+    }
+    return file;
+  }
+
+  /**
+   * load config file.
+   *
+   * @param file config file name.
+   * @return books finders
+   * @throws IOException Error in reading configuration json.
+   */
+  private static Config loadConfig(String file) throws IOException {
+    logger.info("loaing config : {}", file);
+    Config config = Config.getConfig(file);
+    logger.info("loaded config : {} : {}", file, config);
+    return config;
+  }
 }
